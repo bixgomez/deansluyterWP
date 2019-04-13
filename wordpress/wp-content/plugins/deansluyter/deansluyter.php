9c08bae472906419786e5deb1863e7b6526831e4
Richard Gilbert <gilbert67@gmail.com>
2019-04-12 20:09:35 -0700

    New "Dean Sluyter" plugin.

---
+++ wordpress/wp-content/plugins/deansluyter/deansluyter.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:     Deansluyter
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     deansluyter
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Deansluyter
+ */
+
+// Your code starts here.
+
+require_once(dirname(__FILE__) . '/blocks/homepage-callout.php');
