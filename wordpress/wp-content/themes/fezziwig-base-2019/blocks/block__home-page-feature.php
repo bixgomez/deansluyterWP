d3daa0de5e8afa0137891ef6a295c3fc5dfccc15
Richard Gilbert <gilbert67@gmail.com>
2019-04-16 00:28:18 -0700

    Update to previous.

--- wordpress/wp-content/themes/fezziwig-base-2019/blocks/block__home-page-feature.php
+++ wordpress/wp-content/themes/fezziwig-base-2019/blocks/block__home-page-feature.php
@@ -13,15 +13,22 @@ $body = get_field( 'body' );
 $body_extra = get_field( 'extra_text' );
 $url = get_field( 'link' );
 $link_text = get_field( 'link_text' );
-$background_image = get_field( 'featured_image' );
+$featured_image = get_field( 'featured_image' );
+$image_position = get_field( 'image_position' );
 $full_width = get_field( 'full_width' );
 ?>
 
 <?php ?>
 
-<div class="item home-page-feature<?php if( $full_width ) : ?> full-width<?php endif; ?>">
+<div class="item home-page-feature<?php if( $full_width ) : ?> full-width<?php endif; ?> image-position--<?php echo $image_position; ?>">
 
   <a class="home-page-feature--link" href="<?php echo esc_html( $url ) ?>"><?php echo $link_text; ?></a>
+
+  <?php if( !empty($featured_image) && $image_position == 'first' ) : ?>
+    <div class="home-page-feature--featured-image">
+      <?php echo wp_get_attachment_image( $featured_image, 'full' ); ?>
+    </div>
+  <?php endif; ?>
 
   <?php if( !empty( $title ) ) : ?>
     <div class="home-page-feature--title">
@@ -40,17 +47,21 @@ $full_width = get_field( 'full_width' );
     </div>
   <?php endif; ?>
 
+  <?php if( !empty( $featured_image) && $image_position == 'last' ) : ?>
+    <div class="home-page-feature--featured-image">
+      <?php echo wp_get_attachment_image( $featured_image, 'full' ); ?>
+    </div>
+  <?php endif; ?>
+
   <?php if( !empty( $link_text ) ) : ?>
     <div class="home-page-feature--link-text">
       <?php echo $link_text; ?>
     </div>
   <?php endif; ?>
 
-  <?php if( !empty( $background_image ) ) : ?>
+  <?php if( !empty( $featured_image ) && $image_position == 'background' ) : ?>
     <div class="home-page-feature--background-image">
-      <?php
-      echo wp_get_attachment_image( $background_image, 'full' );
-      ?>
+      <?php echo wp_get_attachment_image( $featured_image, 'full' ); ?>
     </div>
   <?php endif; ?>
 
