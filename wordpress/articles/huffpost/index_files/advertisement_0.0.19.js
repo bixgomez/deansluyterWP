02a88d93336073af3dacae86404678dbac8b2ce1
Richard Gilbert <gilbert67@gmail.com>
2021-01-11 18:37:50 -0800

    New "articles" directory for standalone content.

---
+++ wordpress/articles/huffpost/index_files/advertisement_0.0.19.js
@@ -0,0 +1,39 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+document.write('<div id="advertisement-thamba" style="display:none">Display Advertisement</div>');/* Copyright (c) 2020, Yahoo! Inc.  All rights reserved. */
+
+
+}
+/*
+     FILE ARCHIVED ON 04:05:42 Aug 05, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 02:14:44 Jan 12, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots.policy: 274.219
+  exclusion.robots: 274.237
+  CDXLines.iter: 60.877 (3)
+  xauthn.chkprivs: 166.603
+  esindex: 0.025
+  load_resource: 163.0 (2)
+  PetaboxLoader3.datanode: 160.345 (5)
+  xauthn.identify: 107.17
+  captures_list: 1176.736
+  RedisCDXSource: 4.29
+  LoadShardBlock: 212.394 (3)
+  PetaboxLoader3.resolve: 102.145 (3)
+*/
