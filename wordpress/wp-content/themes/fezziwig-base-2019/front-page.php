50a3fe2e5b07a30a297303b084d0314fd67bbce6
Richard Gilbert <gilbert67@gmail.com>
2021-10-24 17:48:17 -0700

    Updates to home page.

--- wordpress/wp-content/themes/fezziwig-base-2019/front-page.php
+++ wordpress/wp-content/themes/fezziwig-base-2019/front-page.php
@@ -15,6 +15,29 @@
 get_header();
 ?>
 
+    <div class="front-page-header--wrapper">
+        <div class="front-page-header">
+            <div class="front-page-image">
+                <div class="front-page-text">
+                    <div class="inner">
+                        <?php while ( have_posts() ) : the_post(); ?>
+                            <?php  ?>
+                            <h1 class="site-name">Dean Sluyter</h1>
+                            <?php  ?>
+                            <p class="home-page-intro-text">
+                                <?php the_field('intro_text'); ?>
+                            </p>
+                        <?php endwhile; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="front-page-cta">
+                <a class="new-book-link" href="https://www.amazon.com/gp/product/1608687694?pf_rd_r=JJ2NA1PDNSYX3CZSGZW2&pf_rd_p=1ab92b69-98d7-4842-a89b-ad387c54783f&pd_rd_r=76f8de66-6693-4503-8989-2b88c69c0b33&pd_rd_w=t0xhQ&pd_rd_wg=V8mRG&ref_=pd_gw_unk"><span class="new-book-link__text">New Book</span></a>
+            </div>
+        </div>
+    </div>
+
+<?php /* ?>
 <div class="front-page-image">
   <div class="front-page-text">
     <div class="inner">
@@ -29,6 +52,7 @@ get_header();
     </div>
   </div>
 </div>
+<?php */ ?>
 
 <?php /* ?>
   <div class="front-page-grid--wrapper">
@@ -41,7 +65,7 @@ get_header();
       ?>
     </div>
   </div>
- <?php */ ?>
+<?php */ ?>
 
 <div class="front-page-content--wrapper">
   <div class="front-page-content">
