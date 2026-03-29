90e6855b1ec4192f5419bb4beac6da71745f3068
Richard Gilbert <gilbert67@gmail.com>
2026-03-28 20:56:27 -0700

    Optimizing home page hero image.

--- web/wp-content/themes/deansluyter-theme-2026/front-page.php
+++ web/wp-content/themes/deansluyter-theme-2026/front-page.php
@@ -13,11 +13,28 @@
  */
 
 get_header();
+
+$hero_image_path = get_template_directory() . '/assets/dean-portrait__optimized.jpg';
+$hero_image_uri  = get_template_directory_uri() . '/assets/dean-portrait__optimized.jpg';
+
+if ( file_exists( $hero_image_path ) ) {
+	$hero_image_uri = add_query_arg( 'ver', (string) filemtime( $hero_image_path ), $hero_image_uri );
+}
 ?>
 
     <div class="front-page-header--wrapper">
         <div class="front-page-header">
-            <div class="front-page-image">
+            <div class="front-page-image" style="background-image: none;">
+                <img
+                    class="front-page-image__media"
+                    src="<?php echo esc_url( $hero_image_uri ); ?>"
+                    alt="<?php echo esc_attr__( 'Portrait of Dean Sluyter', 'deansluyter-theme' ); ?>"
+                    width="1076"
+                    height="768"
+                    loading="eager"
+                    fetchpriority="high"
+                    decoding="async"
+                />
                 <div class="front-page-text">
                     <div class="inner">
                         <?php while ( have_posts() ) : the_post(); ?>
