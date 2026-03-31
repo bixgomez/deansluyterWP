bc29c5ace3ed49beb6c3b230a845a6287e6ae726
Richard Gilbert <gilbert67@gmail.com>
2026-03-31 07:10:51 -0700

    Updated build for blocks.

--- web/wp-content/plugins/deansluyter-blocks/build/blocks/home-page-feature/index.asset.php
+++ web/wp-content/plugins/deansluyter-blocks/build/blocks/home-page-feature/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9775af661748f0596ba8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '377cb85059e567326361');
