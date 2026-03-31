99d03adfadf1263d23763d52792d4cb9c78d5f81
Richard Gilbert <gilbert67@gmail.com>
2026-03-31 08:47:22 -0700

    Updates to book cover.

--- web/wp-content/themes/deansluyter-theme-2026/front-page.php
+++ web/wp-content/themes/deansluyter-theme-2026/front-page.php
@@ -16,9 +16,21 @@ get_header();
 
 $hero_image_path = get_template_directory() . '/assets/dean-portrait__optimized.jpg';
 $hero_image_uri  = get_template_directory_uri() . '/assets/dean-portrait__optimized.jpg';
+$cta_cover_path  = get_template_directory() . '/assets/dharma-bums-cover__optimized.jpg';
+$cta_cover_uri   = get_template_directory_uri() . '/assets/dharma-bums-cover__optimized.jpg';
+$cta_award_path  = get_template_directory() . '/assets/nautilus_award.png';
+$cta_award_uri   = get_template_directory_uri() . '/assets/nautilus_award.png';
 
 if ( file_exists( $hero_image_path ) ) {
 	$hero_image_uri = add_query_arg( 'ver', (string) filemtime( $hero_image_path ), $hero_image_uri );
+}
+
+if ( file_exists( $cta_cover_path ) ) {
+	$cta_cover_uri = add_query_arg( 'ver', (string) filemtime( $cta_cover_path ), $cta_cover_uri );
+}
+
+if ( file_exists( $cta_award_path ) ) {
+	$cta_award_uri = add_query_arg( 'ver', (string) filemtime( $cta_award_path ), $cta_award_uri );
 }
 ?>
 
@@ -49,7 +61,27 @@ if ( file_exists( $hero_image_path ) ) {
                 </div>
             </div>
             <div class="front-page-cta">
-                <a class="new-book-link" rel="nofollow" href="/books/"><span class="new-book-link__text">New Book</span></a>
+                <a class="new-book-link" rel="nofollow" href="/books/">
+                    <img
+                        class="new-book-link__cover"
+                        src="<?php echo esc_url( $cta_cover_uri ); ?>"
+                        alt=""
+                        width="600"
+                        height="928"
+                        decoding="async"
+                        fetchpriority="low"
+                    />
+                    <img
+                        class="new-book-link__award"
+                        src="<?php echo esc_url( $cta_award_uri ); ?>"
+                        alt=""
+                        width="270"
+                        height="267"
+                        loading="lazy"
+                        decoding="async"
+                    />
+                    <span class="new-book-link__text">New Book</span>
+                </a>
             </div>
         </div>
     </div>
